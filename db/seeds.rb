71780fb420d12f863361499fda80e3ca652c5bdd
subinhuang <806519191@qq.com>
2017-02-22 20:23:02 +0800

    modify insdex page & add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-u = User.new
-u.email = "admin@test.com"
+# u = User.new
+# u.email = "admin@test.com"
+#
+# u.password = "123456"
+#
+# u.password_confirmation = "123456"
+#
+# u.is_admin = true
+#
+# u.save
 
-u.password = "123456"
 
-u.password_confirmation = "123456"
+products =[{title:"Apple iPhone 6 64GB",
+	        description: "This Certified Refurbished product has been tested and certified
+	              to work and look like new, with minimal to no signs of wear, by a specialized
+	              third-party seller approved by Amazon. The product is backed by a minimum 90-day warranty,
+	              and may arrive in a generic brown or white box. Accessories may be generic and not directly
+	              from the manufacturer.",
+	        image_path: "#{Rails.root}/app/assets/images/iphone.jpg"},
 
-u.is_admin = true
+	        {title:"A Mid-Back Mesh Chair",
+	        description: "Ergonomic office chair with contoured mesh back for breathability
+	              Pneumatic seat-height adjustment; padded seat for comfort.",
+	        image_path: "#{Rails.root}/app/assets/images/chair.jpg"}
+]
 
-u.save
+u = User.create([ email: 'admin@test.com' ,  password: '123456' ,password_confirmation:'123456',is_admin:true])
+puts "create a adminer"
+
+p1 = Product.create([title:products[0][:title],description:products[0][:description],quantity:20,price:30,image:open(products[0][:image_path])])
+p2 = Product.create([title:products[1][:title],description:products[1][:description],quantity:20,price:30,image:open(products[1][:image_path])])
+puts "create products"
